df985fa2bb40bab67cbcc7d641f0e571b34905a0
nsellek <nick@seansellek.com>
2017-01-14 15:30:52 -0500

    added picture field to product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114182525) do
+ActiveRecord::Schema.define(version: 20170114202955) do
 
   create_table "event_types", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170114182525) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "picture"
   end
 
   create_table "users", force: :cascade do |t|
