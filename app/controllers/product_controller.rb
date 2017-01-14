33909d1d976a9ea06fa4a8bc7720ce239134ddcf
nsellek <nick@seansellek.com>
2017-01-14 15:28:53 -0500

    added buy logic to the product controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,2 +1,19 @@
 class ProductController < ApplicationController
+
+  def buy
+    if BtcAccount.get_balance_for(current_user) >= @product.price
+      BtcTransfer.penalize_user(current_user, @product.price)
+      # TODO: redirect to a purchaced page
+
+    else
+      # TODO: redirect to a page that informs the user he doesnt have enough balance
+      
+    end
+  end
+
+  private
+
+  def grab_product
+    @product = Product.find(params[:id])
+  end
 end
