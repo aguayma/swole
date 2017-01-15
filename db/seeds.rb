644fa7b4602d73f2c5a08efd337cb3bd4b782f56
Mario Aguayo <mario@mystyleblox.com>
2017-01-14 21:42:23 -0500

    added product index styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,14 @@ Anyway, we should be at Alderaan about oh-two-hundred hours. Now be careful, Art
 Screaming about it won't help you. That's 'cause droids don't pull people's arms out of their socket when
 they lose. Wookiees are known to do that. I see your point, sir. I suggest a new strategy, Artoo."
 
-product1 = Product.create(title: '1mo. Crossfit Membership',price: '$69.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/crossfit.jpg
+product1 = Product.create(title: '1mo. Crossfit Membership',price: 70,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/crossfit.jpg
 ')
-product2 = Product.create(title: 'Nikes Trainers',price: '$39.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/nike-shoes.jpeg')
-product3 = Product.create(title: 'P90X Home Workout',price: '$39.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/p90x.jpg')
-product4 = Product.create(title: 'Adidas Running Shorts',price: '$9.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/running-shorts.jpeg')
-product5 = Product.create(title: 'Turbo Socks',price: '$39.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/socks.png')
-product6 = Product.create(title: 'Sport Bottle',price: '$4.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/sports-bottle.jpg')
-product7 = Product.create(title: 'Head Band',price: '$2.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/sweatband.jpg')
-product8 = Product.create(title: 'Nike Gym Bag',price: '$9.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/gym-bag.jpg')
-product9 = Product.create(title: 'Running Pants',price: '$19.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/running-pants.jpeg')
-product10 = Product.create(title: 'Wind Breaker',price: '$39.99',description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/windbreaker.jpg')
+product2 = Product.create(title: 'Nikes Trainers',price: 40,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/nike-shoes.jpeg')
+product3 = Product.create(title: 'P90X Home Workout',price: 40,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/p90x.jpg')
+product4 = Product.create(title: 'Adidas Running Shorts',price: 10,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/running-shorts.jpeg')
+product5 = Product.create(title: 'Turbo Socks',price: 40,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/socks.png')
+product6 = Product.create(title: 'Sport Bottle',price: 5,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/sports-bottle.jpg')
+product7 = Product.create(title: 'Head Band',price: 3,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/sweatband.jpg')
+product8 = Product.create(title: 'Nike Gym Bag',price: 10,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/gym-bag.jpg')
+product9 = Product.create(title: 'Running Pants',price: 20,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/running-pants.jpeg')
+product10 = Product.create(title: 'Wind Breaker',price: 40,description: description, picture: 'https://s3-us-west-2.amazonaws.com/becasted-static-assets/hackathon+images/windbreaker.jpg')
