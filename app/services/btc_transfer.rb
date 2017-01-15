5cf3ab5ee7874dc8b0a01e0fe760e58c28df808b
Jon <jonr87ee@gmail.com>
2017-01-15 04:16:46 -0500

    fix issue with coinbase minimum

--- app/services/btc_transfer.rb
+++ app/services/btc_transfer.rb
@@ -1,11 +1,13 @@
 class BtcTransfer
 
   def self.reward_user(user, amount)
+    return unless amount.to_i > 60000
     btc = CurrencyConverter.satoshi_to_btc(amount.to_s)
     BtcAccount.primary_account.send(to: user.bitcoin_address, amount: btc.to_s , currency: "BTC", description: "Payment")
   end
 
   def self.penalize_user(user, amount)
+    return unless amount.to_i > 60000
     btc = CurrencyConverter.satoshi_to_btc(amount.to_s)
     account = BtcAccount.btc_account_by_id(user.account_id)
     account.send(to: BtcAccount.swole_btc_address, amount: btc.to_s , currency: "BTC", description: "Payment")
