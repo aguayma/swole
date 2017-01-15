a923df8ec06d6b4f30527740ca527011b5413a7c
Mario Aguayo <mario@mystyleblox.com>
2017-01-15 00:42:10 -0500

    more styling changes

--- app/services/event_graph.rb
+++ app/services/event_graph.rb
@@ -1,11 +1,25 @@
 class EventGraph
 
   def self.distance_graph(user)
+    biking = []
+    running = []
+    user.events.each do |event|
+      if event.event_type.code == "CYC"
+        biking << [event.event_date.to_date, event.data.to_f]
+      else
+        running << [event.event_date.to_date, event.data.to_f]
+      end
+    end
+    return [{name: 'BIKING', data: biking}, {name: 'RUNNING', data: running}]
+  end
+
+  def self.earnings_graph(user)
     data = []
     user.events.each do |event|
-      data << [event.event_date.to_date, event.data.to_f]
+      event.goal
+      data << [event.event_date.to_date, event.goal.amount]
     end
-    return [{name: 'miles', data: data}]
+    return [{name: 'SATOSHI', data: data}]
   end
 
 end
